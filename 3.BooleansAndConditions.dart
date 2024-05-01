5e8b8946006c7e1022a0234562495cb3b8b51b45
Jesiel Palacios <jopalacioss@unac.edu.co>
2024-05-01 11:34:04 -0500

    Data types - Booleans and Conditions

---
+++ 3.BooleansAndConditions.dart
@@ -0,0 +1,12 @@
+void main() {
+  bool activated = true;
+
+  print(activated);
+
+  // if (activated == true) {
+  if (activated) {
+    print('The engine is working.');
+  } else {
+    print('The engine is down.');
+  }
+}
