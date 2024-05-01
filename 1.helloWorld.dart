879011e12181956e95ef0c34cf0caf561c71d6a2
Jesiel Palacios <jopalacioss@unac.edu.co>
2024-05-01 11:33:18 -0500

    Hello world in Dart

---
+++ 1.helloWorld.dart
@@ -0,0 +1,18 @@
+void main() {
+  // hi
+
+  /*
+    hi
+  */
+
+  for (int i = 0; i < 5; i++) {
+    print('hello ${i + 1}');
+  }
+
+  // var name = 'Pepito';
+  String name = 'Pepito';
+
+  print('Hello ' + name);
+  print('Hello ${name}');
+  print('Hello $name');
+}
