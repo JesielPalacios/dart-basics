3c718f50ad13b2bf2cd125459436aa42e0ac894e
Jesiel Palacios <jopalacioss@unac.edu.co>
2024-05-01 11:33:36 -0500

    Data types - Numbers and Strings

---
+++ 2.NumbersAndStrings.dart
@@ -0,0 +1,21 @@
+void main() {
+  //
+  // Numbers
+  //
+  int employees = 10;
+  double pi = 3.141592;
+  var number = 1.0;
+
+  print('$employees - $pi - $number');
+
+  //
+  // String - Chain of characters
+  //
+  String name = 'To\'ny';
+  String name2 = "To\'ny";
+
+  print(name);
+  print(name2);
+  print(name2[0]);
+  print(name2[ name2.length - 1]);
+}
